92ec2600a07c5beb6b2557e7189601c507caced6
CharilLiu <liujiale987@outlook.com>
2026-07-29 02:59:14 +0800

    Initial commit

--- Java_SE/src/day07_封装_静态_对象数组/ArraysUtils.java
+++ Java_SE/src/day07_封装_静态_对象数组/ArraysUtils.java
@@ -1,19 +1,13 @@
 package day07_封装_静态_对象数组;
 
 
+import java.util.Arrays;
+
 public class ArraysUtils {
     private ArraysUtils() {
     }
 
     public static void print(int[] arr) {
-        System.out.print("[");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-            if (i != arr.length - 1) {
-                System.out.print(",");
-            } else {
-                System.out.print("]");
-            }
-        }
+        System.out.println(Arrays.toString(arr));
     }
 }
